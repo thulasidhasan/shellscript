180401332b00ab390ee90b87435e2c71fc245c8f
thulasidhasan <44389206+thulasidhasan@users.noreply.github.com>
2018-11-05 15:32:36 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 echo `hostname`
 #test
+
+#################   array example   #######################333
+
+#!/bin/bash
+
+Fruits=('Apple' 'Mango' 'Orange' 'Banana')
+
+Fruits['4']=papaya
+echo "${Fruits[@]}"
+echo "${Fruits[1]}"
+
+#####################################################################
+
+
+
+
